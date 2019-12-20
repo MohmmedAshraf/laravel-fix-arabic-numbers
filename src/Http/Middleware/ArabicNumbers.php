8a2440045ab262b60cede85bced43e20a76b5b4a
Mohamed Ashraf <cupo.ashraf@gmail.com>
2019-12-20 02:31:27 +0000

    Apply fixes from StyleCI

--- src/Http/Middleware/ArabicNumbers.php
+++ src/Http/Middleware/ArabicNumbers.php
@@ -4,7 +4,7 @@ namespace OutheBox\FixArabicNumbers\Http\Middleware;
 
 use Illuminate\Foundation\Http\Middleware\TransformsRequest;
 
-class FixArabicNumbers extends TransformsRequest
+class ArabicNumbers extends TransformsRequest
 {
     /**
      * The attributes that should not be trimmed.
@@ -18,18 +18,20 @@ class FixArabicNumbers extends TransformsRequest
     /**
      * Transform the given value.
      *
-     * @param  string $key
-     * @param  mixed $value
+     * @param string $key
+     * @param mixed  $value
+     *
      * @return mixed
      */
     protected function transform($key, $value)
     {
-        if ((! is_string($value)) || in_array($key, $this->except, true)) {
+        if ((!is_string($value)) || in_array($key, $this->except, true)) {
             return $value;
         }
 
         $eastern = ['٠', '١', '٢', '٣', '٤', '٥', '٦', '٧', '٨', '٩'];
         $western = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+
         return str_replace($eastern, $western, $value);
     }
 }
